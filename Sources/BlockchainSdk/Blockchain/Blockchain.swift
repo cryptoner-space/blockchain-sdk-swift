5ecbfc2d2afc78d82b05e4d554e79b2a0baf5d33
Skibin Alexander <skibinalexander@gmail.com>
2024-03-04 23:18:30 +0300

    Refactor by registry

--- Sources/BlockchainSdk/Blockchain/Blockchain.swift
+++ Sources/BlockchainSdk/Blockchain/Blockchain.swift
@@ -5,7 +5,7 @@ import Foundation
 // MARK: - Blockchain
 
 /// Main structure blockchain Sdk
-public enum Blockchain: String, Codable, CaseIterable, Hashable {
+public enum Blockchain: String, Codable, CaseIterable, Hashable, Identifiable {
     
     // MARK: - Supported
     
@@ -19,18 +19,18 @@ public enum Blockchain: String, Codable, CaseIterable, Hashable {
     case toncoin
     
     /// Arbitrum One ✅
-    case arbitrumOne
+    case arbitrumOne = "arbitrumOne"
     
     // MARK: - Unsupported
     
     /// Ethereum Classic ⭕️
-    case ethereumClassic
+    case ethereumClassic = "classic"
     
     /// Bitcoin ⭕️
     case bitcoin
     
     /// Binance Smart Chain ⭕️
-    case binanceSmartChain
+    case binanceSmartChain = "bsc"
     
     /// Cosmos ⭕️
     case cosmos
@@ -57,13 +57,19 @@ public enum Blockchain: String, Codable, CaseIterable, Hashable {
     case polkadot
     
     /// Bitcoin Cash ⭕️
-    case bitcoinCash
+    case bitcoinCash = "bitcoincash"
     
     /// Dogecoin ⭕️
     case dogecoin
     
     /// Arbitrum ⭕️
     case arbitrum
+    
+    // MARK: - Identifiable
+    
+    public var id: String {
+        self.rawValue
+    }
 
 }
 
@@ -75,28 +81,11 @@ public extension Blockchain {
     }
     
     var displayName: String {
-        self.rawValue.capitalizingFirstLetter()
-    }
-    
-    var tokenTypeName: String? {
         switch self {
-        case .ethereum: return Blockchain.TokenType.ERC20.name
-        case .tron: return Blockchain.TokenType.TRC20.name
+        case .arbitrumOne:
+            return rawValue.capitalizingFirstLetter()
         default:
-            return nil
-        }
-    }
-    
-    var canHandleTokens: Bool {
-        switch self {
-        case .ethereum, 
-                .binance,
-                .solana,
-                .tron,
-                .arbitrumOne:
-            return true
-        default:
-            return false
+            return (try? BlockchainRegistry.shared.resolve(for: self).name) ?? ""
         }
     }
     
@@ -117,16 +106,10 @@ public extension Blockchain {
 extension Blockchain: DecimalValueDescription {
     public var decimalCount: Int {
         switch self {
-        case .bitcoin, .binance, .bitcoinCash, .litecoin, .dogecoin:
-            return 8
-        case .ethereum, .ethereumClassic, .binanceSmartChain, .arbitrumOne:
-            return 18
-        case  .tron, .ripple:
-            return 7
-        case .solana, .toncoin:
-            return 9
+        case .arbitrumOne:
+            return (try? BlockchainRegistry.shared.resolve(for: .arbitrum).decimals) ?? 0
         default:
-            return 0
+            return (try? BlockchainRegistry.shared.resolve(for: self).decimals) ?? 0
         }
     }
     
@@ -142,14 +125,36 @@ extension Blockchain: DecimalValueDescription {
 // MARK: - Coin Resolve
 
 public extension Blockchain {
-    var coin: Blockchain.Coin? {
+    func resolveCoin(by id: String) -> Blockchain.Coin? {
         Blockchain.Coin.allCases.first(where: { $0.blockchain == self })
     }
 }
 
-// MARK: - TokenType
+// MARK: - Tokens
 
 public extension Blockchain {
+    var tokenTypeName: String? {
+        switch self {
+        case .ethereum: return Blockchain.TokenType.ERC20.name
+        case .tron: return Blockchain.TokenType.TRC20.name
+        default:
+            return nil
+        }
+    }
+    
+    var canHandleTokens: Bool {
+        switch self {
+        case .ethereum,
+                .binance,
+                .solana,
+                .tron,
+                .arbitrumOne:
+            return true
+        default:
+            return false
+        }
+    }
+    
     enum TokenType: String, Codable {
         case ERC20, BEP20, TRC20
         
